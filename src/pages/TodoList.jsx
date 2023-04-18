0267d2dbaafa7ed8e00211eda3c3f03b820af1b4
Ahn <tn3748@naver.com>
2023-04-19 06:00:21 +0900

    feat: todo list 목록 마크업 구현

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,9 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ROUTES } from "../constants";
 import { useNavigate } from "react-router-dom";
+import AddTodo from "../components/AddTodo";
+import Todo from "../components/Todo";
 
 function TodoList() {
   const navigate = useNavigate();
+  const [todos, setTodos] = useState([
+    { id: "123", text: "장보기" },
+    { id: "124", text: "공부하기" },
+    { id: "125", text: "코딩하기" },
+  ]);
+
+  const handleAddTodo = (value) => setTodos((prev) => [...prev, value]);
 
   useEffect(() => {
     if (!localStorage.getItem("accessToken")) {
@@ -13,9 +22,14 @@ function TodoList() {
   }, []);
 
   return (
-    <div>
-      <div>투두</div>
-    </div>
+    <main>
+      <ul>
+        {todos.map((item) => (
+          <Todo key={item.id} todo={item} />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAddTodo} />
+    </main>
   );
 }
 
