f81c4dff27812b2467905c4e57282cbe1dea78e1
Ahn <tn3748@naver.com>
2023-04-19 17:28:46 +0900

    feat: 수정 기능 속성 부여 및 활성화 구현

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 function Todo({ item, onDelete }) {
+  const [isModify, setModify] = useState(false);
   const { id, todo } = item;
 
   return (
     <li>
       <input type="checkbox" id={id} />
-      <label htmlFor={id}>{todo}</label>
-      <button data-testid="modify-button">수정</button>
-      <button data-testid="delete-button" onClick={() => onDelete(id)}>
-        삭제
-      </button>
+
+      {isModify ? (
+        <>
+          <input type="text" value={todo} data-testid="modify-input" />
+          <button data-testid="submit-button">제출</button>
+          <button data-testid="cancel-button" onClick={() => setModify(false)}>
+            취소
+          </button>
+        </>
+      ) : (
+        <>
+          <label htmlFor={id}>{todo}</label>
+          <button data-testid="modify-button" onClick={() => setModify(true)}>
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={() => onDelete(id)}>
+            삭제
+          </button>
+        </>
+      )}
     </li>
   );
 }
